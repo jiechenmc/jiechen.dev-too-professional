d3096e579ab22b785610c0728cb16a3e03c56f82
Jie Chen <jiechenmc@gmail.com>
2024-04-30 14:39:29 -0400

    invert github logo

--- components/home/LinkBar.tsx
+++ components/home/LinkBar.tsx
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/react';
 const LinkBar = () => {
     return <div className="grid grid-flow-col gap-4">
         <Link href="/github" target="_blank" >
-            <Icon icon="logos:github-icon" width="31" height="34" />
+            <Icon className="dark:invert" icon="logos:github-icon" width="31" height="34" />
         </Link>
         <Link href="/resume" target="_blank">
             <Icon icon="mdi:resume" width="31" height="34" />

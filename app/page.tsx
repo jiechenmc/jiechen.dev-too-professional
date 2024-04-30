f3d3dce9c9310bad0a64399e81427363eb3f5b01
Jie Chen <jiechenmc@gmail.com>
2024-04-30 14:32:59 -0400

    publish dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,11 @@ import LinkBar from "@/components/home/LinkBar";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <TopBanner lText="" rText="" />
+      <TopBanner lText="Rewriting my website right now; will finish after finals!!" />
 
       <div className="relative  flex flex-col gap-5 place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
-          className="relative z-[-1] dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert select-none"
+          className="relative z-[-1] dark:drop-shadow-[0_0_0.3rem_#ffffff70] select-none"
           src="/self.jpg"
           alt="Jie Chen selfie"
           width={256}
@@ -30,7 +30,7 @@ export default function Home() {
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <NavItem title="About" href="/about" body="Learn more about me :D" />
         <NavItem title="Projects" href="/projects" body="Check out some projects I have done in the past." />
-        <NavItem title="Blog" href="https://blog.jiechen.dev/posts" body="Check out some tutorials that I have made for others and myself." />
+        <NavItem title="Blog" href="https://blog.jiechen.dev" body="Check out some tutorials that I have made for others and myself." />
         <NavItem title="Loqi" href="https://loqi.jiechen.dev" body="An generative AI study application that I am currently working on with my Vertically Integrated Projects (VIP) Team" />
       </div>
     </main >
--- components/home/TopBanner.tsx
+++ components/home/TopBanner.tsx
@@ -1,8 +1,9 @@
+import ModeToggle from "./ModeToggle"
+
 interface Props {
     lText: string
-    rText: string
 }
-const TopBanner = ({ lText, rText }: Props) => {
+const TopBanner = ({ lText }: Props) => {
     return (
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
             {/* Top Left */}
@@ -11,9 +12,7 @@ const TopBanner = ({ lText, rText }: Props) => {
             </p>
             <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
                 {/* Top Right */}
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    {rText}
-                </p>
+                <ModeToggle />
             </div>
         </div>)
 }

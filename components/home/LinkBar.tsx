1136b1a0cd6a6a65ef48b4ae9124fba2f56f9c9f
Jie Chen <jiechenmc@gmail.com>
2024-04-30 19:50:16 -0400

    update

--- components/home/LinkBar.tsx
+++ components/home/LinkBar.tsx
@@ -3,13 +3,13 @@ import { Icon } from '@iconify/react';
 
 const LinkBar = () => {
     return <div className="grid grid-flow-col gap-4">
-        <Link href="/github" target="_blank" >
+        <Link className="hover:drop-shadow-[0_0_0.3rem_#ffffffff]" href="/github" target="_blank" >
             <Icon className="dark:invert" icon="logos:github-icon" width="31" height="34" />
         </Link>
-        <Link href="/resume" target="_blank">
+        <Link className="hover:drop-shadow-[0_0_0.3rem_#ffffffff]" href="/resume" target="_blank">
             <Icon icon="mdi:resume" width="31" height="34" />
         </Link>
-        <Link href="/linkedin" target="_blank">
+        <Link className="hover:drop-shadow-[0_0_0.3rem_#ffffffff]" href="/linkedin" target="_blank">
             <Icon icon="logos:linkedin-icon" width="31" height="34" />
         </Link>
     </div>
